16f3c32ec40a53599e94fae55096703d6f04843a
alansastre <alan@alansastre.co>
2021-08-06 11:26:42 +0200

    fix import

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.domain.Car;
 import com.example.demo.domain.Employee;
 import com.example.demo.service.EmployeeService;
 
